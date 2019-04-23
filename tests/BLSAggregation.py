220f6c7d11202087d109987ca7cdde31f4694899
0xAshish <a4ashishrp@gmail.com>
2019-04-23 14:51:32 +0530

    BLS POC solidity

--- tests/BLSAggregation.py
+++ tests/BLSAggregation.py
@@ -6,15 +6,13 @@ def testKeyAggregation():
     privKey1 = 31
     privKey2 = 35
     privKey3 = 37
-
-    privKey4 = privKey1 + privKey2 + privKey3
+    x,y =G1,G2
 
     pubKey1 = multiply(G2, privKey1)
     pubKey2 = multiply(G2, privKey2)
     pubKey3 = multiply(G2, privKey3)
 
 
-    pubKey4 = multiply(G2, privKey4)
 
     pubKeyAgg = add(add(pubKey1, pubKey2), pubKey3)
 
@@ -25,7 +23,6 @@ def testKeyAggregation():
     sign2 = multiply(H, privKey2)
     sign3 = multiply(H, privKey3)
 
-    sign4 = multiply(H, privKey4)
 
     aggSign = add(add(sign1, sign2), sign3)
 
@@ -33,10 +30,6 @@ def testKeyAggregation():
     pairing2 = pairing(G2, aggSign)
     assert pairing1 == pairing2  # test sign Aggregation
 
-    pairing3 = pairing(G2, sign4)
-    assert sign4 == aggSign  # test privkey Aggregation
-
-    assert pairing1 == pairing3
     print("yey")
 
 testKeyAggregation()
